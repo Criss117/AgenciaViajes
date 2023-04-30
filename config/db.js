c59d92ebf0bd02918bbe3a3182d3d03395b468d5
Criss117 <cristianviveros@gmail.com>
2023-04-30 13:27:54 -0500

    fix db.js planetscale

--- config/db.js
+++ config/db.js
@@ -5,6 +5,11 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: true,        
+        }
+    },
     define:{
         timestamps: false
     },
